0a348e77bd1de15f45dd90bc7a687bd8f8091b6f
Cem Asma <dev.cemas@gmail.com>
2016-11-11 04:48:12 +0300

    filter done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var key = "62CBEFE74FE6CFD96089DADE782FB7B8";
-var http = new XMLHttpRequest();
-
+var req = new XMLHttpRequest();
+var coURL = "https://crossorigin.me/";
 
 function query(userid) {
 	games = get_user_games(get_steamid(userid));
@@ -8,13 +8,29 @@ function query(userid) {
 }
 
 function get_user_games(steamid) {
-	
+	req.open("GET", coURL + "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1?key="+
+			key+"&steamid="+steamid+"&include_appinfo=1&include_played_free_games=1", false);
+	req.send(null);
+	return JSON.parse(req.responseText).response.games;
 }
 
 function get_steamid(userid) {
-	http.open("GET", "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001?key="+key+"&vanityurl="+userid);
-	http.send();
-	console.log(http.responseText);
+	req.open("GET", coURL + "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001?key="+key+"&vanityurl="+userid, false);
+	req.send(null);
+	return JSON.parse(req.responseText).response.steamid;
 }
 
-get_steamid("nazgrolvi");
+function get_linux_games(games) {
+	req.open("GET", coURL + "https://raw.githubusercontent.com/SteamDatabase/SteamLinux/master/GAMES.json", false);
+	req.send(null);
+	linux_supported_games = [];
+	linux_games = JSON.parse(req.responseText);
+	for(var i = 0; i < games.length; i++) {
+		if(linux_games.hasOwnProperty(games[i].appid)) {
+			linux_supported_games.push(games[i]);
+		}
+	}
+	return linux_supported_games;
+}
+
+console.log(query("nazgrolvi"));
