588d774a8eb36c449f5eb47ce86f643d5042ffc6
Cem Asma <dev.cemas@gmail.com>
2016-11-12 02:46:12 +0300

    requests changed as async

--- js/script.js
+++ js/script.js
@@ -3,44 +3,57 @@ var req = new XMLHttpRequest();
 var coURL = "https://crossorigin.me/";
 
 function query(userid) {
-	games = get_user_games(get_steamid(userid));
-	return { "games": games, "linux_supported_games": get_linux_games(games) };
+	req.onreadystatechange = handler;
+	startFlowWithSteamId(userid);
 }
 
-function get_user_games(steamid) {
-	req.open("GET", coURL + "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1?key="+
-			key+"&steamid="+steamid+"&include_appinfo=1&include_played_free_games=1", false);
-	req.send(null);
-	return JSON.parse(req.responseText).response.games;
-}
-
-function get_steamid(userid) {
-	req.open("GET", coURL + "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001?key="+key+"&vanityurl="+userid, false);
-	req.send(null);
-	return JSON.parse(req.responseText).response.steamid;
-}
-
-function get_linux_games(games) {
-	req.open("GET", coURL + "https://raw.githubusercontent.com/SteamDatabase/SteamLinux/master/GAMES.json", false);
-	req.send(null);
-	linux_supported_games = [];
-	linux_games = JSON.parse(req.responseText);
-	for(var i = 0; i < games.length; i++) {
-		if(linux_games.hasOwnProperty(games[i].appid)) {
-			linux_supported_games.push(games[i]);
+function handler(e) {
+	if(req.readyState === 4 && req.status === 200) {
+		if(e.currentTarget.responseURL.indexOf("ISteamUser") > -1) {
+			userGamesRequest(JSON.parse(req.responseText).response.steamid);
+		} else if(e.currentTarget.responseURL.indexOf("IPlayerService") > -1) {
+			this.games = JSON.parse(req.responseText).response.games;
+			linuxGamesRequest();
+		} else {
+			scrapGames(this.games, JSON.parse(req.responseText));
 		}
 	}
-	return linux_supported_games;
 }
 
-function button_click() {
-	context = query(document.getElementById("userid").value);
-	gamesDiv = document.getElementById("games");
-	gamesDiv.innerHTML = "";
-	for(var i = 0; i < context.linux_supported_games.length; i++) {
-		if(context.linux_supported_games[i].img_logo_url) {
-			gamesDiv.innerHTML += "<img src='http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/"+
-				context.linux_supported_games[i].appid+"/"+context.linux_supported_games[i].img_logo_url+".jpg'/>";
+function startFlowWithSteamId(userid) {
+	req.open("GET", coURL + "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001?key="+key+"&vanityurl="+userid, true);
+	req.send();
+}
+
+function userGamesRequest(steamid) {
+	req.open("GET", coURL + "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1?key="+
+			key+"&steamid="+steamid+"&include_appinfo=1&include_played_free_games=1", true);
+	req.send();
+}
+
+function linuxGamesRequest() {
+	req.open("GET", coURL + "https://raw.githubusercontent.com/SteamDatabase/SteamLinux/master/GAMES.json", true);
+	req.send();	
+}
+
+function scrapGames(games, linux_games) {
+	var linuxSupportedGames = [];
+	for(var i = 0; i < games.length; i++) {
+		if(linux_games.hasOwnProperty(games[i].appid)) {
+			linuxSupportedGames.push(games[i]);
+			addToDiv(document.getElementById("games"), games[i].appid, games[i].img_logo_url);
 		}
+	}
+}
+
+function buttonClick() {
+	gamesDiv = document.getElementById("games").innerHTML = "";
+	gamesDiv.innerHTML = "";
+	query(document.getElementById("userid").value);
+}
+
+function addToDiv(div, appid, img_logo_url) {
+	if(img_logo_url) {
+		div.innerHTML += "<img src='http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/"+appid+"/"+img_logo_url+".jpg'/>";
 	}
 }
